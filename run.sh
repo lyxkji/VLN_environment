63785889a065a46186fae133a1a2d19570402666
lyxkji <105537076+lyxkji@users.noreply.github.com>
2025-03-26 16:51:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ export PROJECT_DIR=/path/to/your_project
 export HABITAT_DATA_DIR=/path/to/v1/tasks/mp3d
 
 
-docker run --gpus all --shm-size=50g -it\
+docker run --gpus all --name myvln --shm-size=50g -it\
     --mount type=bind,source=$MATTERPORT_DATA_DIR,target=/root/mount/Matterport3DSimulator/data/v1/scans \
     --mount type=bind,source=$MATTERPORT_SIMULATOR_DIR,target=/root/mount/Matterport3DSimulator \
     --mount type=bind,source=$PROJECT_DIR,target=/root/mount/your_project \
     --mount type=bind,source=$HABITAT_DATA_DIR,target=/root/mount/Matterport3DSimulator/data/scene_datasets/mp3d \
-    myvln
+    crpi-7t2wiqt0bx1eq6eo.cn-shanghai.personal.cr.aliyuncs.com/vln/vln_mp3d_habiat_pytorch:v2
